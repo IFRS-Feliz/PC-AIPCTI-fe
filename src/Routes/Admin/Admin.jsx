22ef75ff33b00219d1e5b5d06e931576ef8b8d07
Rafael Scholz Griebler <rafaelscholz03@gmail.com>
2021-04-26 17:51:41 -0300

    Botao da pagina de projetos na home do Admin

--- src/Routes/Admin/Admin.jsx
+++ src/Routes/Admin/Admin.jsx
@@ -1,18 +1,14 @@
-import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
 
 import style from "../../assets/css/routes/admin.module.css";
 
 export default function Admin() {
-  const history = useHistory();
   return (
-    <div>
-      <h1 className={style.tituloAdmin}>Admin</h1>
+    <div className={style.actualContainerAdmin}>
+      <h1>Admin</h1>
       <div className={style.containerAdmin}>
-        <div className={style.centralizar}>
-          <button
-            className={style.botaoAdmin}
-            onClick={() => history.push("/admin/usuarios")}
-          >
+        <div className={style.opcoesAdmin}>
+          <Link className={style.botaoAdmin} to={"/admin/usuarios"}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               height="24px"
@@ -24,15 +20,12 @@ export default function Admin() {
               <path d="M0 0h24v24H0V0z" fill="none" />
               <path d="M12 6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2m0 10c2.7 0 5.8 1.29 6 2H6c.23-.72 3.31-2 6-2m0-12C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 10c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
             </svg>
-          </button>
+          </Link>
           <p className={style.pAdmin}>Usuários</p>
         </div>
-        <div className={style.separador}></div>
-        <div className={style.centralizar}>
-          <button
-            className={style.botaoAdmin}
-            onClick={() => history.push("/admin/editais")}
-          >
+        <div className={style.separador} />
+        <div className={style.opcoesAdmin}>
+          <Link className={style.botaoAdmin} to={"/admin/editais"}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               height="24px"
@@ -44,8 +37,24 @@ export default function Admin() {
               <path d="M0 0h24v24H0V0z" fill="none" />
               <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z" />
             </svg>
-          </button>
+          </Link>
           <p className={style.pAdmin}>Editais</p>
+        </div>
+        <div className={style.separador} />
+        <div className={style.opcoesAdmin}>
+          <Link className={style.botaoAdmin} to={"/admin/projetos"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#000000"
+              className={style.svgAdmin}
+            >
+              <path d="M9,4v1.38c-0.83-0.33-1.72-0.5-2.61-0.5c-1.79,0-3.58,0.68-4.95,2.05l3.33,3.33h1.11v1.11c0.86,0.86,1.98,1.31,3.11,1.36 V15H6v3c0,1.1,0.9,2,2,2h10c1.66,0,3-1.34,3-3V4H9z M7.89,10.41V8.26H5.61L4.57,7.22C5.14,7,5.76,6.88,6.39,6.88 c1.34,0,2.59,0.52,3.54,1.46l1.41,1.41l-0.2,0.2c-0.51,0.51-1.19,0.8-1.92,0.8C8.75,10.75,8.29,10.63,7.89,10.41z M19,17 c0,0.55-0.45,1-1,1s-1-0.45-1-1v-2h-6v-2.59c0.57-0.23,1.1-0.57,1.56-1.03l0.2-0.2L15.59,14H17v-1.41l-6-5.97V6h8V17z" />
+            </svg>
+          </Link>
+          <p className={style.pAdmin}>Projetos</p>
         </div>
       </div>
     </div>
